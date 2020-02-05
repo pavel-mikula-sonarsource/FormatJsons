3a7b3eb0a17e12bc1cd672014009576028988cfc
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-02-05 10:34:51 +0100

    Add project files.

---
+++ FormatJsons/Code/Main.vb
@@ -0,0 +1,33 @@
+﻿
+Imports System.IO
+
+Module MainMod
+
+    Sub Main(ParamArray Args() As String)
+        Try
+            For Each Arg As String In Args
+                If Directory.Exists(Arg) Then
+                    Console.WriteLine("Processing: " & Arg)
+                    Process(Arg)
+                Else
+                    Console.WriteLine("Directory not found: " & Arg)
+                End If
+            Next
+            Console.WriteLine("Done")
+        Catch ex As Exception
+            Console.WriteLine($"ERROR {ex.GetType.ToString}: {ex.Message}")
+            Console.WriteLine()
+            Console.WriteLine(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Process(Root As String)
+        For Each DN As String In Directory.GetDirectories(Root)
+            Process(DN)
+        Next
+        For Each FN As String In Directory.GetFiles(Root, "*.json")
+
+        Next
+    End Sub
+
+End Module
