84a71dae90e3efe96b7b6805cf8858e7b1ba29a4
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-02-05 11:02:06 +0100

    .netcore

--- FormatJsons/Code/Program.vb
+++ FormatJsons/Code/Program.vb
@@ -1,9 +1,10 @@
-﻿
+
 Imports System.IO
+Imports System.Text.Json
 
-Module MainMod
+Module Program
 
-    Sub Main(ParamArray Args() As String)
+    Sub Main(Args As String())
         Try
             For Each Arg As String In Args
                 If Directory.Exists(Arg) Then
@@ -22,11 +23,17 @@ Module MainMod
     End Sub
 
     Private Sub Process(Root As String)
+        Dim jDoc As JsonDocument, aSdf As String
         For Each DN As String In Directory.GetDirectories(Root)
             Process(DN)
         Next
         For Each FN As String In Directory.GetFiles(Root, "*.json")
-
+            aSdf = File.ReadAllText(FN)
+            aSdf = aSdf.Replace("'", """")
+            jDoc = JsonDocument.Parse(aSdf, New JsonDocumentOptions With {.AllowTrailingCommas = True})
+            Using fs As New FileStream(FN, FileMode.Open), wr As New Utf8JsonWriter(fs, New JsonWriterOptions With {.Indented = True})
+                jDoc.WriteTo(wr)
+            End Using
         Next
     End Sub
 
